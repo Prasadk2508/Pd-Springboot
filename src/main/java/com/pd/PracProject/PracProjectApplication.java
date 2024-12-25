5522b73e42b8d849490e71b6f4ff807d169f5ce6
prasad <pra2508sad@gmail.com>
2024-12-25 18:42:56 +0530

    Greetings added

--- src/main/java/com/pd/PracProject/PracProjectApplication.java
+++ src/main/java/com/pd/PracProject/PracProjectApplication.java
@@ -2,12 +2,14 @@ package com.pd.PracProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.origin.SystemEnvironmentOrigin;
 
 @SpringBootApplication
 public class PracProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracProjectApplication.class, args);
+		System.out.println("Hi pd here..");
 	}
 
 }
