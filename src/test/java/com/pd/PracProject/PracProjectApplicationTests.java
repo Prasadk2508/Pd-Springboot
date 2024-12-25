4e495f536f104beab1d3b4028818beb1719e5c14
prasad <pra2508sad@gmail.com>
2024-12-25 18:14:40 +0530

    Pd's First Commit

---
+++ src/test/java/com/pd/PracProject/PracProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pd.PracProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
